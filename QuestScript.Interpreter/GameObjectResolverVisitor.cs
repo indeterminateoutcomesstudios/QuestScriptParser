1714c855442cd0cd95059ace2fae2546e26a2bd0
Michael Yarichuk <michael.yarichuk@gmail.com>
2018-11-21 11:01:46 +0200

    minor stuff (detect whether aslx is a game or a library)

--- QuestScript.Interpreter/GameObjectResolverVisitor.cs
+++ QuestScript.Interpreter/GameObjectResolverVisitor.cs
@@ -5,10 +5,16 @@ using QuestScript.Parser;
 
 namespace QuestScript.Interpreter
 {
+    public enum GameFileType
+    {
+        Game,
+        Library
+    }
+
     public class GameObjectResolverVisitor : QuestGameParserBaseVisitor<bool>
     {
         public HashSet<string> References { get; } = new HashSet<string>();
-
+        public GameFileType Type { get; private set; }
         public override bool VisitElement(QuestGameParser.ElementContext context)
         {
             if (string.IsNullOrWhiteSpace(context.ElementName?.Text)) //precaution
@@ -18,7 +24,14 @@ namespace QuestScript.Interpreter
                 case "include":
                     VisitInclude(context);
                     break;                    
+                case "library":
+                    Type = GameFileType.Library;
+                    break;                    
+                case "asl":
+                    Type = GameFileType.Game;
+                    break;                    
             }
+
             return base.VisitElement(context);
         }
 
@@ -27,7 +40,7 @@ namespace QuestScript.Interpreter
             var refAttribute = context.attribute().FirstOrDefault(attr => attr.Key.Text.Equals("ref"));
             if (refAttribute == null)
             {
-                throw new InvalidDataException("Found <include> element, but didn't find 'ref' attribute. This should not happen, and it is likely this ASLX game file is corrupted.");
+                throw new InvalidDataException("Found <include> element, but didn't find 'ref' attribute. This should not happen, and it is likely this ASLX game file is corrupted. The element I found is : " + context.GetText());
             }
 
             References.Add(refAttribute.Value.Text.CleanTokenArtifacts());
